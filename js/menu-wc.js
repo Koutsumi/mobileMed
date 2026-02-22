d56699d8800f07806c74b790e6ce2277ea11b525
Koutsumi <72818130+Koutsumi@users.noreply.github.com>
2026-02-22 20:34:03 +0000

    Deploying to gh-pages from @ Koutsumi/mobileMed@ee8ea4e45c70526ed5137708f2d25d7ee2219dcc 🚀

---
+++ js/menu-wc.js
@@ -0,0 +1,159 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">Playwright Test Project</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                                <li class="link">
+                                    <a href="overview.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-keypad"></span>Overview
+                                    </a>
+                                </li>
+
+                            <li class="link">
+                                <a href="index.html" data-type="chapter-link">
+                                    <span class="icon ion-ios-paper"></span>
+                                        README
+                                </a>
+                            </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+
+                    </ul>
+                </li>
+                    <li class="chapter additional">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#additional-pages"'
+                            : 'data-bs-target="#xs-additional-pages"' }>
+                            <span class="icon ion-ios-book"></span>
+                            <span>QA Report</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="additional-pages"' : 'id="xs-additional-pages"' }>
+                                    <li class="link ">
+                                        <a href="additional-documentation/readme.html" data-type="entity-link" data-context-id="additional">README</a>
+                                    </li>
+                                    <li class="link ">
+                                        <a href="additional-documentation/shared-globals.html" data-type="entity-link" data-context-id="additional">Shared Globals</a>
+                                    </li>
+                                    <li class="link ">
+                                        <a href="additional-documentation/qa-report.html" data-type="entity-link" data-context-id="additional">QA Report</a>
+                                    </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#classes-links"' :
+                            'data-bs-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/ExamsPage.html" data-type="entity-link" >ExamsPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/LayoutPage.html" data-type="entity-link" >LayoutPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/LoginPage.html" data-type="entity-link" >LoginPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/ManageExamDialogPage.html" data-type="entity-link" >ManageExamDialogPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/ManagePatientDialogPage.html" data-type="entity-link" >ManagePatientDialogPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/PatientsPage.html" data-type="entity-link" >PatientsPage</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/DbUsersystem.html" data-type="entity-link" >DbUsersystem</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/IAuthLoginRequest.html" data-type="entity-link" >IAuthLoginRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/IExamRequest.html" data-type="entity-link" >IExamRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/IPacienteRequest.html" data-type="entity-link" >IPacienteRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/IUserSystemRequest.html" data-type="entity-link" >IUserSystemRequest</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
